c300e271e5c739344fd344e68f59a65b867ec8db
NegativeTier <helpryzencloud@gmail.com>
2026-07-12 16:21:52 +0530

    Update PlayerListHudMixin.java

--- src/main/java/fun/crystalplus/tiertagger/mixin/PlayerListHudMixin.java
+++ src/main/java/fun/crystalplus/tiertagger/mixin/PlayerListHudMixin.java
@@ -24,7 +24,7 @@ public abstract class PlayerListHudMixin {
         PlayerListEntry entry,
         CallbackInfoReturnable<Text> cir
     ) {
-        String username = entry.getProfile().asString();
+        String username = entry.getProfile().getName();
 
         Map<String, String> tiers =
             TierTaggerClient.getPlayerTiers(username);
